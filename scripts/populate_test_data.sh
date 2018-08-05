8ef90c9e2b9c592062f871cf501f880d52f2e498
Terrence Cole <terrence.d.cole@gmail.com>
2018-08-04 20:12:19 -0700

    First pass a lib test setup script.

--- scripts/populate_test_data.sh
+++ scripts/populate_test_data.sh
@@ -7,34 +7,14 @@ then
     exit 1
 fi
 
-if [ $# -eq 0 ]
-then
-    echo "No arguments supplied"
-    echo "Expected the directory containing unpacked libs"
-    exit 1
-fi
+#mkdir -p test_data/{un,}packed/{USNF,MF,ATF,ATFNF,ATFG,USNF97,FA}
 
-SH_FILES=`find "$1" -name "*.SH"`
-PAL_FILES=`find "$1" -name "*.PAL"`
-PIC_FILES=`find "$1" -name "*.PIC"`
-ENT_FILES=`find "$1" -name "*.[ONPJ]T"`
-OT_FILES=`find "$1" -name "*.OT"`
-NT_FILES=`find "$1" -name "*.NT"`
-PT_FILES=`find "$1" -name "*.PT"`
-JT_FILES=`find "$1" -name "*.JT"`
+for GAME in $(ls --color=never test_data/packed/);
+do
+    LIBS=`ls --color=never test_data/packed/$GAME/* | xargs -n1 realpath`
+    mkdir -p test_data/unpacked/$GAME
 
-mkdir -p libs/sh/test_data
-mkdir -p libs/pal/test_data
-mkdir -p libs/pic/test_data
-mkdir -p libs/peff/test_data
-mkdir -p libs/entity/test_data
-mkdir -p libs/[onpj]t/test_data
-
-cp -v $ENT_FILES libs/ot/test_data/
-cp -v $NT_FILES libs/nt/test_data/
-cp -v $PT_FILES libs/nt/test_data/
-cp -v $PT_FILES libs/pt/test_data/
-cp -v $JT_FILES libs/jt/test_data/
-cp -v $SH_FILES libs/sh/test_data/
-cp -v $PAL_FILES libs/pal/test_data/
-cp -v $PIC_FILES libs/pic/test_data/
+    pushd libs/lib
+    cargo run --release -- unpack -o ../../test_data/unpacked/$GAME $LIBS
+    popd
+done
