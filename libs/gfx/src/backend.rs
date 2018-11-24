76328bf50045cb2aaabbee0502289e2a319dcedc
Terrence Cole <terrence.d.cole@gmail.com>
2018-11-24 08:20:35 -0800

    gfx-rs is just to immature to be usable for hobbyist work yet.

--- libs/gfx/src/backend.rs
+++
@@ -1,6 +0,0 @@
-#[cfg(feature = "dx12")]
-pub(crate) use gfx_backend_dx12 as backend;
-#[cfg(feature = "metal")]
-pub(crate) use gfx_backend_metal as backend;
-#[cfg(feature = "vulkan")]
-pub(crate) use gfx_backend_vulkan as backend;
