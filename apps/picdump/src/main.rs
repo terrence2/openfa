7c6276dc8b2b13039fce1a0dc90c37801d60790f
Terrence Cole <terrence.d.cole@gmail.com>
2019-03-23 09:14:48 -0700

    Add a tool to dump palettes from pics and use them in other pics.

---
+++ apps/picdump/src/main.rs
@@ -0,0 +1,104 @@
+// This file is part of OpenFA.
+//
+// OpenFA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenFA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
+use failure::Fallible;
+use omnilib::{OmniLib, make_opt_struct};
+use pal::Palette;
+use pic::Pic;
+use std::{fs, iter};
+use structopt::StructOpt;
+
+make_opt_struct!(#[structopt(
+    name = "picdump",
+    about = "Extract PICs to PNG files and show PIC metadata"
+)]
+Opt {
+    #[structopt(
+        short = "s",
+        long = "show-palette",
+        help = "Dump the palette here as a PNG"
+    )]
+    show_palette => Option<String>,
+
+    #[structopt(
+        short = "d",
+        long = "dump-palette",
+        help = "Dump the palette here as a PAL"
+    )]
+    dump_palette => Option<String>,
+
+    #[structopt(
+        short = "u",
+        long = "use-palette",
+        help = "Use the given palette when decoding"
+    )]
+    use_palette => Option<String>,
+
+    #[structopt(
+        short = "w",
+        long = "write",
+        help = "Write the image to the given file"
+    )]
+    write_image => Option<String>
+});
+
+fn main() -> Fallible<()> {
+    let opt = Opt::from_args();
+
+    let (omni, inputs) = opt.find_inputs()?;
+    if inputs.is_empty() {
+        println!("No inputs found!");
+        return Ok(());
+    }
+
+    for (game, name) in &inputs {
+        let lib = omni.library(&game);
+        let content = lib.load(&name)?;
+        let image = Pic::new(&content)?;
+
+        println!("{}:{}", game, name);
+        println!(
+            "{}",
+            iter::repeat("=")
+                .take(1 + game.len() + name.len())
+                .collect::<String>()
+        );
+        println!("format: {:?}", image.format);
+        println!("width:  {}px", image.width);
+        println!("height: {}px", image.height);
+        if let Some(pal) = image.palette {
+            println!("colors: {:?}", pal.color_count);
+
+            if let Some(ref path) = opt.show_palette {
+                pal.dump_png(&path)?;
+            }
+
+            if let Some(ref path) = opt.dump_palette {
+                pal.dump_pal(&path)?;
+            }
+        }
+
+        if let Some(target) = &opt.write_image {
+            let palette = if let Some(path) = &opt.use_palette {
+                Palette::from_bytes(&fs::read(path)?)?
+            } else {
+                Palette::from_bytes(&lib.load("PALETTE.PAL")?)?
+            };
+            let image = Pic::decode(&palette, &content)?;
+            image.save(target)?;
+        }
+    }
+
+    Ok(())
+}
