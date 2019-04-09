1a79525d987eef939ac38f790930a35749d3a98d
Terrence Cole <terrence.d.cole@gmail.com>
2019-04-08 19:28:37 -0700

    Convert unlib to structopt for great savings.

--- src/bin/unlib.rs
+++ src/bin/unlib.rs
@@ -14,115 +14,108 @@
 // along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
 
 // Unpack lib files.
-use clap::{App, Arg, SubCommand};
-use failure::Error;
+use failure::Fallible;
 use humansize::{file_size_opts as options, FileSize};
 use std::{
     fs::{create_dir, remove_file, File},
     io::Write,
-    path::Path,
+    path::{Path, PathBuf},
 };
+use structopt::StructOpt;
 
-fn main() -> Result<(), Error> {
-    let matches = App::new("FightersAnthology engine lib unpacker")
-        .version("0.1")
-        .author("Terrence Cole <terrence.d.cole@gmail.com>")
-        .about("List and unpack part or all of an EA lib file or files")
-        .subcommand(
-            SubCommand::with_name("ls")
-                .about("list the contents of a lib")
-                .arg(
-                    Arg::with_name("INPUT")
-                        .help("Sets the input libs to unpack")
-                        .required(true)
-                        .multiple(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("unpack")
-                .about("unpack the given lib files")
-                .arg(
-                    Arg::with_name("output")
-                        .long("--output")
-                        .short("-o")
-                        .required(true)
-                        .takes_value(true)
-                        .value_name("DIR")
-                        .help("output into the given directory"),
+#[derive(Debug, StructOpt)]
+#[structopt(name = "unpack")]
+/// A LIB unpacking tool for all Janes Fighters Anthology series games
+enum Opt {
+    #[structopt(name = "ls")]
+    /// List the contents of a lib
+    List {
+        #[structopt(parse(from_os_str))]
+        /// The lib files to list
+        inputs: Vec<PathBuf>,
+    },
+
+    #[structopt(name = "unpack")]
+    /// Unpack the given lib file
+    Unpack {
+        #[structopt(short = "-o", long = "--output", parse(from_os_str))]
+        /// Output unpacked libs into this directory
+        output_path: PathBuf,
+
+        #[structopt(parse(from_os_str))]
+        /// The lib files to unpack
+        inputs: Vec<PathBuf>,
+    },
+}
+
+fn main() -> Fallible<()> {
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::List { inputs } => handle_ls(inputs),
+        Opt::Unpack {
+            inputs,
+            output_path,
+        } => handle_unpack(inputs, output_path),
+    }
+}
+
+fn handle_ls(inputs: Vec<PathBuf>) -> Fallible<()> {
+    let multi_input = inputs.len() > 1;
+    for (i, input) in inputs.iter().enumerate() {
+        let libfile = lib::Library::from_paths(&[Path::new(input).to_owned()])?;
+        if multi_input {
+            if i != 0 {
+                println!();
+            }
+            println!("{:?}:", input);
+        }
+        for name in libfile.find_matching("*")?.iter() {
+            let info = libfile.stat(name)?;
+            let mut psize = info.packed_size.file_size(options::BINARY).unwrap();
+            if psize.ends_with(" B") {
+                psize += "  ";
+            }
+            let mut asize = info.unpacked_size.file_size(options::BINARY).unwrap();
+            if asize.ends_with(" B") {
+                asize += "  ";
+            }
+            let ratio = if info.packed_size == info.unpacked_size {
+                "~".to_owned()
+            } else {
+                format!(
+                    "{:0.3}x",
+                    info.packed_size as f64 / info.unpacked_size as f64
                 )
-                .arg(
-                    Arg::with_name("INPUT")
-                        .help("Sets the input libs to unpack")
-                        .required(true)
-                        .multiple(true),
-                ),
-        )
-        .get_matches();
+            };
 
-    if let Some(matches) = matches.subcommand_matches("ls") {
-        let inputs = matches.values_of("INPUT").unwrap();
-        let multi_input = inputs.len() > 1;
-        for (i, input) in inputs.enumerate() {
-            let libfile = lib::Library::from_paths(&[Path::new(input).to_owned()])?;
-            if multi_input {
-                if i != 0 {
-                    println!();
-                }
-                println!("{}:", input);
-            }
-            for name in libfile.find_matching("*")?.iter() {
-                let info = libfile.stat(name)?;
-                let mut psize = info.packed_size.file_size(options::BINARY).unwrap();
-                if psize.ends_with(" B") {
-                    psize += "  ";
-                }
-                let mut asize = info.unpacked_size.file_size(options::BINARY).unwrap();
-                if asize.ends_with(" B") {
-                    asize += "  ";
-                }
-                let ratio = if info.packed_size == info.unpacked_size {
-                    "~".to_owned()
-                } else {
-                    format!(
-                        "{:0.3}x",
-                        info.packed_size as f64 / info.unpacked_size as f64
-                    )
-                };
-
-                println!(
-                    "{:15} {:?} {:>12} {:>12}  {}",
-                    info.name, info.compression, psize, asize, ratio
-                );
-            }
+            println!(
+                "{:15} {:?} {:>12} {:>12}  {}",
+                info.name, info.compression, psize, asize, ratio
+            );
         }
     }
 
-    if let Some(matches) = matches.subcommand_matches("unpack") {
-        let inputs = matches.values_of("INPUT").expect("no inputs specified");
-        let outname: String = matches
-            .value_of("output")
-            .expect("no output specified")
-            .to_owned();
-        let output = Path::new(&outname);
-        for input in inputs {
-            let libname = Path::new(input)
-                .file_name()
-                .expect("no filename in library");
-            let outdir = output.join(libname);
-            let libfile = lib::Library::from_paths(&[Path::new(input).to_owned()])?;
-            if !outdir.exists() {
-                create_dir(&outdir)?;
+    Ok(())
+}
+
+fn handle_unpack(inputs: Vec<PathBuf>, output_path: PathBuf) -> Fallible<()> {
+    for input in &inputs {
+        let libname = input.file_name().expect("no filename in library");
+        let outdir = output_path.join(libname);
+        let libfile = lib::Library::from_paths(&[Path::new(input).to_owned()])?;
+        if !outdir.exists() {
+            create_dir(&outdir)?;
+        }
+        for name in libfile.find_matching("*")?.iter() {
+            let outfilename = outdir.join(name);
+            println!("{:?}:{} -> {:?}", input, name, outfilename);
+            let content = libfile.load(name)?;
+            if outfilename.exists() {
+                remove_file(&outfilename)?;
             }
-            for name in libfile.find_matching("*")?.iter() {
-                let outfilename = outdir.join(name);
-                println!("{}:{} -> {:?}", input, name, outfilename);
-                let content = libfile.load(name)?;
-                if outfilename.exists() {
-                    remove_file(&outfilename)?;
-                }
-                let mut fp = File::create(outfilename)?;
-                fp.write_all(&content)?;
-            }
+            let mut fp = File::create(outfilename)?;
+            fp.write_all(&content)?;
         }
     }
 
