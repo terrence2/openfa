2358b5733e7487b322363255a7c3aab710086820
Terrence Cole <terrence.d.cole@gmail.com>
2019-04-20 09:00:25 -0700

    Switch paldump to structopt

--- apps/paldump/src/main.rs
+++ apps/paldump/src/main.rs
@@ -12,38 +12,35 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
-use clap::{App, Arg};
-use failure::Error;
+use failure::{bail, Fallible};
+use omnilib::{make_opt_struct, OmniLib};
 use pal::Palette;
-use std::{fs, io::Read};
+use structopt::StructOpt;
 
-fn main() -> Result<(), Error> {
-    let matches = App::new("OpenFA palette dumper")
-        .version("0.0.1")
-        .author("Terrence Cole <terrence.d.cole@gmail.com>")
-        .about("Figure out what bits belong where.")
-        .arg(
-            Arg::with_name("dump")
-                .long("--dump")
-                .short("-d")
-                .help("dump the palette to a png")
-                .takes_value(false)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("POSITIONS")
-                .help("The palette indexes to dump")
-                .multiple(true)
-                .required(false),
-        )
-        .get_matches();
+make_opt_struct!(
+    #[structopt(name = "paldump", about = "Show the contents of a PAL file")]
+    Opts {
+        #[structopt(short = "d", long = "dump", help = "Dump the palette to a png")]
+        dump => bool,
 
-    let mut fp = fs::File::open("test_data/PALETTE.PAL")?;
-    let mut data = Vec::new();
-    fp.read_to_end(&mut data)?;
-    let pal = Palette::from_bytes(&data)?;
+        #[structopt(short = "p", long = "position", help = "Show the color at this offset offset")]
+        position => Option<String>
+    }
+);
 
-    if matches.is_present("dump") {
+fn main() -> Fallible<()> {
+    let opt = Opts::from_args();
+
+    let (omni, inputs) = opt.find_inputs()?;
+    if inputs.is_empty() {
+        bail!("no inputs");
+    }
+
+    let (game, name) = inputs.first().unwrap();
+    let lib = omni.library(&game);
+    let pal = Palette::from_bytes(&lib.load(name)?)?;
+
+    if opt.dump {
         let size = 80;
         let mut buf = image::ImageBuffer::new(16u32 * size, 16u32 * size);
         for i in 0..16 {
@@ -62,15 +59,13 @@ fn main() -> Result<(), Error> {
         return Ok(());
     }
 
-    if let Some(positions) = matches.values_of("POSITIONS") {
-        for pos_str in positions {
-            let pos = if pos_str.starts_with("0x") {
-                usize::from_str_radix(&pos_str[2..], 16)?
-            } else {
-                pos_str.parse::<usize>()?
-            };
-            println!("{:02X} => {:?}", pos, pal.rgb(pos)?);
-        }
+    if let Some(pos_str) = opt.position {
+        let pos = if pos_str.starts_with("0x") {
+            usize::from_str_radix(&pos_str[2..], 16)?
+        } else {
+            pos_str.parse::<usize>()?
+        };
+        println!("{:02X} => {:?}", pos, pal.rgb(pos)?);
     }
 
     Ok(())
