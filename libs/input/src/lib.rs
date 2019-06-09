920426ea6977ec61b9e6d88ab63c3a783d10afd9
Terrence Cole <terrence.d.cole@gmail.com>
2019-06-09 08:44:33 -0700

    Push the strongly typed version as far as possible.

--- libs/input/src/lib.rs
+++ libs/input/src/lib.rs
@@ -25,13 +25,17 @@ use winit::{
     ScanCode, VirtualKeyCode, WindowEvent, WindowId,
 };
 
-// Map from key, buttons, and axes to commands.
-pub struct InputBindings {
-    pub name: String,
-    chords: HashMap<Key, Vec<(KeySet, &'static str)>>,
+pub trait CommandType {
+    //fn from_str(command: &str) -> (CommandType, Option<CommandType>);
 }
 
-impl InputBindings {
+// Map from key, buttons, and axes to commands.
+pub struct InputBindings<T: CommandType + Copy + Sync + Sized + Send> {
+    pub name: String,
+    chords: HashMap<Key, Vec<(KeySet, T, Option<T>)>>,
+}
+
+impl<T: CommandType + Copy + Sync + Sized + Send> InputBindings<T> {
     pub fn new(name: &str) -> Self {
         Self {
             name: name.to_owned(),
@@ -39,13 +43,22 @@ impl InputBindings {
         }
     }
 
-    pub fn bind(mut self, command: &'static str, keyset: &str) -> Fallible<Self> {
+    pub fn bind_state(mut self, keyset: &str, activate: T, deactivate: T) -> Fallible<Self> {
         for ks in KeySet::from_virtual(keyset)?.drain(..) {
-            // TODO parse command into up-down
             self.chords
                 .entry(ks.activating())
                 .or_insert_with(Vec::new)
-                .push((ks, command));
+                .push((ks, activate, Some(deactivate)));
+        }
+        Ok(self)
+    }
+
+    pub fn bind_event(mut self, keyset: &str, activate: T) -> Fallible<Self> {
+        for ks in KeySet::from_virtual(keyset)?.drain(..) {
+            self.chords
+                .entry(ks.activating())
+                .or_insert_with(Vec::new)
+                .push((ks, activate, None));
         }
         Ok(self)
     }
@@ -55,26 +68,20 @@ impl InputBindings {
         code: VirtualKeyCode,
         state: ElementState,
         keycode_states: &HashMap<VirtualKeyCode, ElementState>,
-    ) -> Option<Command> {
+    ) -> Option<Command<T>> {
         if let Some(chords) = self.chords.get(&Key::Virtual(code)) {
-            for (chord, command) in chords {
+            for (chord, activate, deactivate) in chords {
                 if state == ElementState::Pressed
                     && Self::chord_is_pressed(&chord.keys, keycode_states)
                 {
-                    return Some(Command::new(command));
+                    return Some(Command::new(*activate));
                 }
 
-                if state == ElementState::Released
+                if deactivate.is_some()
+                    && state == ElementState::Released
                     && Self::chord_is_pressed(&chord.keys[..chord.keys.len() - 1], keycode_states)
                 {
-                    // use std::borrow::Cow;
-                    // let cmd = if command.starts_with("+") {
-                    //     Cow::from("-".to_owned() + &command[1..])
-                    // } else {
-                    //     Cow::from(command)
-                    // };
-                    // return Some(Command::new(cmd));
-                    return Some(Command::new(command));
+                    return Some(Command::new(deactivate.unwrap()));
                 }
             }
         }
@@ -99,82 +106,7 @@ impl InputBindings {
     }
 }
 
-#[derive(Clone, Debug)]
-pub enum Arg {
-    Device(DeviceId),
-    Float(f64),
-}
-
-impl Arg {
-    pub fn float(&self) -> Fallible<f64> {
-        match self {
-            Arg::Float(v) => Ok(*v),
-            _ => bail!("argument is not a float"),
-        }
-    }
-}
-
-impl From<f64> for Arg {
-    fn from(v: f64) -> Self {
-        Arg::Float(v)
-    }
-}
-
-impl From<f32> for Arg {
-    fn from(v: f32) -> Self {
-        Arg::Float(f64::from(v))
-    }
-}
-
-impl From<DeviceId> for Arg {
-    fn from(v: DeviceId) -> Self {
-        Arg::Device(v)
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Args {
-    a0: Option<Arg>,
-    a1: Option<Arg>,
-}
-
-impl Args {
-    pub fn empty() -> Self {
-        Args { a0: None, a1: None }
-    }
-
-    pub fn one(a0: Arg) -> Self {
-        Args {
-            a0: Some(a0),
-            a1: None,
-        }
-    }
-
-    pub fn two(a0: Arg, a1: Arg) -> Self {
-        Args {
-            a0: Some(a0),
-            a1: Some(a1),
-        }
-    }
-
-    pub fn first(&self) -> Fallible<Arg> {
-        if let Some(ref a) = self.a0 {
-            return Ok(a.clone());
-        }
-        bail!("not enough arguments: 1 of 0")
-    }
-
-    pub fn second(&self) -> Fallible<Arg> {
-        if let Some(ref a) = self.a1 {
-            return Ok(a.clone());
-        }
-        bail!(
-            "not enough arguments: 2 of {}",
-            if self.a0.is_some() { 1 } else { 0 }
-        )
-    }
-}
-
+/*
 impl From<LogicalSize> for Args {
     fn from(v: LogicalSize) -> Self {
         Args::two(v.width.into(), v.height.into())
@@ -187,62 +119,47 @@ impl From<LogicalPosition> for Args {
     }
 }
 
-impl From<Arg> for Args {
-    fn from(v: Arg) -> Self {
-        Args::one(v)
-    }
-}
-
-impl From<DeviceId> for Args {
-    fn from(v: DeviceId) -> Self {
-        Args::one(v.into())
-    }
-}
-
 impl From<(f64, f64)> for Args {
     fn from(v: (f64, f64)) -> Self {
         Args::two(v.0.into(), v.1.into())
     }
 }
+*/
 
 #[derive(Clone, Debug)]
-pub struct Command {
-    pub name: &'static str,
-    pub args: Args,
+pub enum CommandArg {
+    None,
+    Position(LogicalPosition),
+    Size(LogicalSize),
 }
 
-impl Command {
-    // pub fn empty() -> Self {
-    //     Self {
-    //         name: "empty",
-    //         args: Args::empty(),
-    //     }
-    // }
+#[derive(Clone, Debug)]
+pub struct Command<T: CommandType + Send + Sized + Sync> {
+    pub name: T,
+    pub arg: CommandArg,
+}
 
-    pub fn new(name: &'static str) -> Self {
+impl<T: CommandType + Copy + Send + Sized + Sync> Command<T> {
+    pub fn new(name: T) -> Self {
         Self {
             name,
-            args: Args::empty(),
+            arg: CommandArg::None,
         }
     }
 }
 
-pub fn cmd(name: &'static str, args: Args) -> Option<Command> {
-    Some(Command { name, args })
-}
-
-pub struct InputSystem<'a> {
+pub struct InputSystem<'a, T: CommandType + Copy + Send + Sized + Sync> {
     // Prioritized list of input binding sets. The last set with a matching
     // input binding "wins" and determines the command that is sent for that
     // input event.
-    bindings: Vec<&'a InputBindings>,
+    bindings: Vec<&'a InputBindings<T>>,
 
     // Track key states so that we can match button combos.
     keycodes_state: HashMap<VirtualKeyCode, ElementState>,
     scancodes_state: HashMap<ScanCode, ElementState>,
 }
 
-impl<'a> InputSystem<'a> {
+impl<'a, T: CommandType + Copy + Send + Sized + Sync> InputSystem<'a, T> {
     pub fn empty() -> Self {
         Self {
             bindings: Vec::new(),
@@ -251,7 +168,7 @@ impl<'a> InputSystem<'a> {
         }
     }
 
-    pub fn new(bindings: &[&'a InputBindings]) -> Self {
+    pub fn new(bindings: &[&'a InputBindings<T>]) -> Self {
         Self {
             bindings: bindings.to_owned(),
             keycodes_state: HashMap::new(),
@@ -259,7 +176,7 @@ impl<'a> InputSystem<'a> {
         }
     }
 
-    pub fn push_bindings(mut self, bindings: &'a InputBindings) -> Self {
+    pub fn push_bindings(mut self, bindings: &'a InputBindings<T>) -> Self {
         self.bindings.push(bindings);
         self
     }
@@ -269,7 +186,7 @@ impl<'a> InputSystem<'a> {
         self
     }
 
-    pub fn poll(&mut self, events: &mut EventsLoop) -> SmallVec<[Command; 8]> {
+    pub fn poll(&mut self, events: &mut EventsLoop) -> SmallVec<[Command<T>; 8]> {
         let mut output = SmallVec::new();
         events.poll_events(|e| {
             if let Some(c) = self.handle_event(e) {
@@ -279,7 +196,7 @@ impl<'a> InputSystem<'a> {
         output
     }
 
-    pub fn handle_event(&mut self, e: Event) -> Option<Command> {
+    pub fn handle_event(&mut self, e: Event) -> Option<Command<T>> {
         match e {
             Event::WindowEvent { window_id, event } => self.handle_window_event(window_id, event),
             Event::DeviceEvent { device_id, event } => self.handle_device_event(device_id, event),
@@ -290,10 +207,10 @@ impl<'a> InputSystem<'a> {
         }
     }
 
-    fn handle_window_event(&self, window_id: WindowId, event: WindowEvent) -> Option<Command> {
+    fn handle_window_event(&self, window_id: WindowId, event: WindowEvent) -> Option<Command<T>> {
         //println!("WINDOW EVENT ({:?}): {:?}", window_id, event);
         match event {
-            WindowEvent::Resized(s) => cmd("window-resize", s.into()),
+            //WindowEvent::Resized(s) => cmd("window-resize", s.into()),
             _ => {
                 warn!("unknown window event: {:?}", event);
                 None
@@ -301,15 +218,23 @@ impl<'a> InputSystem<'a> {
         }
     }
 
-    fn handle_device_event(&mut self, device_id: DeviceId, event: DeviceEvent) -> Option<Command> {
+    fn handle_device_event(
+        &mut self,
+        device_id: DeviceId,
+        event: DeviceEvent,
+    ) -> Option<Command<T>> {
         //println!("DEVICE EVENT ({:?}): {:?}", device_id, event);
         match event {
-            DeviceEvent::Added => cmd("device-added", device_id.into()),
-            DeviceEvent::Removed => cmd("device-removed", device_id.into()),
+            // DeviceEvent::Added => cmd("device-added", device_id.into()),
+            // DeviceEvent::Removed => cmd("device-removed", device_id.into()),
+
+            // Mouse Motion
             DeviceEvent::MouseMotion { delta } => cmd("mouse-move", delta.into()),
-            DeviceEvent::MouseWheel {
-                delta: MouseScrollDelta::LineDelta(x, y),
-            } => cmd("mouse-wheel", Args::two(x.into(), y.into())),
+
+            // DeviceEvent::MouseWheel {
+            //     delta: MouseScrollDelta::LineDelta(x, y),
+            // } => cmd("mouse-wheel", Args::two(x.into(), y.into())),
+
             // Match virtual keycodes.
             DeviceEvent::Key(KeyboardInput {
                 virtual_keycode: Some(code),
@@ -321,6 +246,7 @@ impl<'a> InputSystem<'a> {
                 self.keycodes_state.insert(code, state);
                 self.match_keycode(code, state)
             }
+
             // Match scancodes.
             DeviceEvent::Key(KeyboardInput {
                 virtual_keycode: None,
@@ -331,6 +257,7 @@ impl<'a> InputSystem<'a> {
                 self.scancodes_state.insert(scancode, state);
                 None
             }
+
             // Duplicate from MouseMotion for some reason?
             DeviceEvent::Motion { .. } => None,
             _ => {
@@ -340,7 +267,7 @@ impl<'a> InputSystem<'a> {
         }
     }
 
-    fn match_keycode(&self, code: VirtualKeyCode, state: ElementState) -> Option<Command> {
+    fn match_keycode(&self, code: VirtualKeyCode, state: ElementState) -> Option<Command<T>> {
         for bindings in self.bindings.iter().rev() {
             if let Some(c) = bindings.match_keycode(code, state, &self.keycodes_state) {
                 return Some(c);
@@ -396,44 +323,60 @@ mod test {
         }
     }
 
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    enum MenuCommands {
+        SelectMenu(bool),
+        Exit,
+    }
+    impl CommandType for MenuCommands {}
+    use MenuCommands as MC;
+
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    enum FpsCommands {
+        MoveForward(bool),
+        Eject,
+    }
+    impl CommandType for FpsCommands {}
+    use FpsCommands as FC;
+
     #[test]
-    fn test_can_handle_common_events() -> Fallible<()> {
-        let fps = InputBindings::new("fps")
-            .bind("+moveforward", "W")?
-            .bind("+moveleft", "a")?
-            .bind("+moveback", "S")?
-            .bind("+moveright", "d")?
-            .bind("eject", "shift+e")?;
-        let mut input = InputSystem::new(&[&fps]);
+    fn test_can_handle_nested_events() -> Fallible<()> {
+        let menu = InputBindings::<MC>::new("fps")
+            .bind_state("alt", MC::SelectMenu(true), MC::SelectMenu(false))?
+            .bind_event("shift+e", MC::Exit)?;
+        let fps = InputBindings::<FC>::new("fps")
+            .bind_state("w", FC::MoveForward(true), FC::MoveForward(false))?
+            .bind_event("shift+e", FC::Eject)?;
+        let mut input = InputSystem::new(&[&menu, &fps]);
 
-        let cmd = input
-            .handle_event(win_evt(WindowEvent::Resized(logical_size())))
-            .unwrap();
-        assert_eq!(cmd.name, "window-resize");
-        assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
+        // let cmd = input
+        //     .handle_event(win_evt(WindowEvent::Resized(logical_size())))
+        //     .unwrap();
+        // assert_eq!(cmd.name, "window-resize");
+        // assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
 
-        let cmd = input
-            .handle_event(dev_evt(DeviceEvent::MouseMotion { delta: (10., 10.) }))
-            .unwrap();
-        assert_eq!(cmd.name, "mouse-move");
-        assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
+        // let cmd = input
+        //     .handle_event(dev_evt(DeviceEvent::MouseMotion { delta: (10., 10.) }))
+        //     .unwrap();
+        // assert_eq!(cmd.name, "mouse-move");
+        // assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
 
-        let cmd = input
-            .handle_event(dev_evt(DeviceEvent::MouseWheel {
-                delta: MouseScrollDelta::LineDelta(10., 10.),
-            }))
-            .unwrap();
-        assert_eq!(cmd.name, "mouse-wheel");
-        assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
+        // let cmd = input
+        //     .handle_event(dev_evt(DeviceEvent::MouseWheel {
+        //         delta: MouseScrollDelta::LineDelta(10., 10.),
+        //     }))
+        //     .unwrap();
+        // assert_eq!(cmd.name, "mouse-wheel");
+        // assert_relative_eq!(cmd.args.first()?.float()?, 10f64);
 
         let cmd = input
             .handle_event(dev_evt(DeviceEvent::Key(vkey(VirtualKeyCode::W, true))))
             .unwrap();
-        assert_eq!(cmd.name, "+moveforward");
+        assert_eq!(cmd.name, FC::MoveForward(true));
         let cmd = input
             .handle_event(dev_evt(DeviceEvent::Key(vkey(VirtualKeyCode::W, false))))
             .unwrap();
-        assert_eq!(cmd.name, "-moveforward");
+        assert_eq!(cmd.name, FC::MoveForward(false));
 
         Ok(())
     }
@@ -441,27 +384,23 @@ mod test {
     #[test]
     fn test_poll_events() -> Fallible<()> {
         let mut window = GraphicsWindow::new(&GraphicsConfigBuilder::new().build())?;
-        let fps = InputBindings::new("fps")
-            .bind("+moveforward", "W")?
-            .bind("+moveleft", "A")?
-            .bind("+moveback", "S")?
-            .bind("+moveright", "D")?;
+        let fps = InputBindings::<FC>::new("fps")
+            .bind_state("w", FC::MoveForward(true), FC::MoveForward(false))?
+            .bind_event("shift+e", FC::Eject)?;
         let mut input = InputSystem::new(&[&fps]);
         input.poll(&mut window.events_loop);
         Ok(())
     }
 
     #[test]
-    //#[ignore]
+    #[ignore]
     fn test_run_forever() -> Fallible<()> {
         use simplelog::{Config, LevelFilter, TermLogger};
         TermLogger::init(LevelFilter::Trace, Config::default())?;
         let mut window = GraphicsWindow::new(&GraphicsConfigBuilder::new().build())?;
-        let fps = InputBindings::new("fps")
-            .bind("+moveforward", "W")?
-            .bind("+moveleft", "A")?
-            .bind("+moveback", "S")?
-            .bind("+moveright", "D")?;
+        let fps = InputBindings::<FC>::new("fps")
+            .bind_state("w", FC::MoveForward(true), FC::MoveForward(false))?
+            .bind_event("shift+e", FC::Eject)?;
         let mut input = InputSystem::new(&[&fps]);
         loop {
             let evt = input.poll(&mut window.events_loop);
