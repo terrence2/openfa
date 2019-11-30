5f16599111a02cedbabab85a61db71fac813dcd1
Terrence Cole <terrence.d.cole@gmail.com>
2019-11-30 15:09:47 -0800

    This comes along, almost by accident.

---
+++ libs/render-wgpu/common/shader_globals/src/lib.rs
@@ -0,0 +1,28 @@
+// This file is part of OpenFA.
+//
+// OpenFA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenFA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
+
+// Our shared shader includes expect certain bind groups to be in certain spots.
+// Note that these are not unique because we need to stay under 4 and thus re-use heavily.
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum Group {
+    Globals = 0,
+    Atmosphere = 2,
+}
+
+impl Group {
+    pub fn index(self) -> u32 {
+        self as u32
+    }
+}
