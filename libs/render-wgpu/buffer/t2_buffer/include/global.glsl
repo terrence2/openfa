bd4fdeef693b7fa53052168cb7acddd0c9938712
Terrence Cole <terrence.d.cole@gmail.com>
2019-12-01 09:50:20 -0800

    Forklift the t2 descriptorset.

--- libs/render-wgpu/buffer/t2_buffer/include/global.glsl
+++ libs/render-wgpu/buffer/t2_buffer/include/global.glsl
@@ -12,23 +12,12 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
-#version 450
-#include <common/shader_globals/include/global.glsl>
-#include <buffer/t2_buffer/include/global.glsl>
 
-#include <buffer/atmosphere/include/global.glsl>
-#include <buffer/atmosphere/include/descriptorset.glsl>
-#include <buffer/atmosphere/include/library.glsl>
+layout(set = 2, binding = 0) uniform texture2D t2_terrain_atlas_texture;
+layout(set = 2, binding = 1) uniform sampler t2_terrain_atlas_sampler;
 
-layout(location = 0) in vec4 v_color;
-layout(location = 1) in vec2 v_tex_coord;
-
-layout(location = 0) out vec4 f_color;
-
-void main() {
-    if (v_tex_coord.x == 0.0) {
-        discard;
-    } else {
-        f_color = t2_atlas_color_uv(v_tex_coord);
-    }
+vec4
+t2_atlas_color_uv(vec2 tex_coord)
+{
+    return texture(sampler2D(t2_terrain_atlas_texture, t2_terrain_atlas_sampler), tex_coord);
 }
