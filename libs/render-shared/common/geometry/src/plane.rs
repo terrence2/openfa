4fa8d944197ffbb0ad3d50380444a2da207170bf
Terrence Cole <terrence.d.cole@gmail.com>
2020-03-08 10:15:53 -0700

    Basic primitives.

--- libs/render-shared/common/geometry/src/plane.rs
+++ libs/render-shared/common/geometry/src/plane.rs
@@ -12,14 +12,77 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with OpenFA.  If not, see <http://www.gnu.org/licenses/>.
-use nalgebra::{Point3, RealField, Vector3, Vector4};
+use approx::relative_eq;
+use nalgebra::{Point3, RealField, Vector3};
 
 #[derive(Debug)]
-pub struct Plane<T: RealField>(Vector4<T>);
+pub struct Plane<T: RealField> {
+    normal: Vector3<T>,
+    d: T,
+}
 
 impl<T: RealField> Plane<T> {
-    pub fn from_point_and_normal(p: Point3<T>, n: Vector3<T>) -> Self {
-        let d = p.coords.dot(&n);
-        Self(Vector4::new(p.coords[0], p.coords[1], p.coords[2], d))
+    pub fn from_point_and_normal(p: &Point3<T>, n: &Vector3<T>) -> Self {
+        Self {
+            normal: n.to_owned(),
+            d: p.coords.dot(n),
+        }
+    }
+
+    pub fn point_on_plane(&self, p: &Point3<T>) -> bool {
+        relative_eq!(self.normal.dot(&p.coords) - self.d, T::zero())
+    }
+
+    pub fn distance(&self, p: &Point3<T>) -> T {
+        self.normal.dot(&p.coords) - self.d
+    }
+
+    pub fn closest_point_on_plane(&self, p: &Point3<T>) -> Point3<T> {
+        p - (self.normal * self.distance(p))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn test_point_on_plane() {
+        let plane = Plane::from_point_and_normal(
+            &Point3::new(0f64, 0f64, 0f64),
+            &Vector3::new(0f64, 0f64, 1f64),
+        );
+        assert!(plane.point_on_plane(&Point3::new(10f64, 10f64, 0f64)));
+        assert!(!plane.point_on_plane(&Point3::new(10f64, 10f64, 0.1f64)));
+        assert!(!plane.point_on_plane(&Point3::new(10f64, 10f64, -0.1f64)));
+    }
+
+    #[test]
+    fn test_point_distance() {
+        let plane = Plane::from_point_and_normal(
+            &Point3::new(0f64, 0f64, 0f64),
+            &Vector3::new(0f64, 0f64, 1f64),
+        );
+
+        assert_relative_eq!(-1f64, plane.distance(&Point3::new(1f64, 1f64, -1f64)));
+        assert_relative_eq!(1f64, plane.distance(&Point3::new(-1f64, -1f64, 1f64)));
+    }
+
+    #[test]
+    fn test_closest_point_on_plane() {
+        let plane = Plane::from_point_and_normal(
+            &Point3::new(0f64, 0f64, 0f64),
+            &Vector3::new(0f64, 0f64, 1f64),
+        );
+
+        assert_relative_eq!(
+            Point3::new(1f64, 1f64, 0f64),
+            plane.closest_point_on_plane(&Point3::new(1f64, 1f64, -1f64))
+        );
+        assert_relative_eq!(
+            Point3::new(-1f64, -1f64, 0f64),
+            plane.closest_point_on_plane(&Point3::new(-1f64, -1f64, 1f64))
+        );
     }
 }
